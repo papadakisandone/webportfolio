1b2015ebe5f80026ccec9efa7978cde45b632871
Antonios Papadakis <83516016+papadakisandone@users.noreply.github.com>
2022-05-27 10:00:26 +0200

    change about text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -37,12 +37,9 @@ const About = () => {
                         </article>
                     </div>
                     <p>
-                        There are many variations of passages of Lorem Ipsum available, 
-                        but the majority have suffered alteration in some form, by injected humour, 
-                        or randomised words which don't look even slightly believable. If you are going
-                        to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing
-                        hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to 
-                        repeat predefined chunks as necessary, making this the first true generator on the Internet.
+                        Hi, my name is Antonios Papadakis. A Web Developer - Programmer  passionate about creating interactive
+                        applications and experiences on web. My base is in beautiful Hamburg, seens i moved here in the last months.
+                        This is a portfolio of my personal work. 
                     </p>
                     <a href="#contact" className="btn btn-primary">Let's Talk</a> 
                 </div>
